b150c73c1d32be92e9842a337f86d366875aba3c
Kehap <dahalkeshap1996@gmail.com>
2022-10-02 20:27:03 +0600

    updates

--- Assets/Script/menuMovement.cs
+++ Assets/Script/menuMovement.cs
@@ -7,17 +7,33 @@ public class menuMovement : MonoBehaviour
 
     public GameObject MenuOrigPos;
     public GameObject MenuActivePos;
-     public GameObject MenuPanel;
+     public  GameObject MenuPanel;
+     public bool Move_Menu_Panel;
+     public bool Move_Menu_Panel_Back;
 
     // Start is called before the first frame update
     void Start()
     {
-       MenuPanel.transform.position = MenuOrigPos.transform.position; 
+       MenuPanel.transform.position = MenuOrigPos.transform.position;
+       
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void MovePanel(){
+Move_Menu_Panel= true;
+Move_Menu_Panel_Back= false;
+
+// Debug.Log(Move_Menu_Panel + "  " + Move_Menu_Panel_Back );
+
+    }
+      public void MovePanelBack(){
+Move_Menu_Panel= false;
+Move_Menu_Panel_Back= true;
+// Debug.Log(Move_Menu_Panel_Back + "  " + Move_Menu_Panel );
     }
 }
